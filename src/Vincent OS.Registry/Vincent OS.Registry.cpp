e84994c7701d8f27ffe65ccecdcdde085bd97101
v38armageddon <69727502+v38armageddon@users.noreply.github.com>
2022-11-04 15:26:17 +0100

    Ajout du Projet : Vincent OS.Registry

---
+++ src/Vincent OS.Registry/Vincent OS.Registry.cpp
@@ -0,0 +1,4 @@
+#include "pch.h"
+
+#include "Vincent OS.Registry.h"
+
---
+++ src/Vincent OS.Registry/Vincent OS.Registry.h
@@ -0,0 +1,13 @@
+#pragma once
+
+using namespace System;
+using namespace Microsoft::Win32;
+
+#undef MessageBox
+
+namespace VincentOSRegistry {
+	public ref class Class1
+	{
+		Registry::CurrentUser
+	};
+}
