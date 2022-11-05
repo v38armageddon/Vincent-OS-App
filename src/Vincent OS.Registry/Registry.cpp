a2b3c9827159a254847ba72edff61d6f45f43b5e
v38armageddon <69727502+v38armageddon@users.noreply.github.com>
2022-11-05 13:33:31 +0100

    Commencement du Registre

--- src/Vincent OS.Registry/Registry.cpp
+++ src/Vincent OS.Registry/Registry.cpp
@@ -1,6 +1,15 @@
 #include "pch.h"
 #include "Registry.h"
 
-int main() {
+void GetInfo() {
+	// Get the Key and value for HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon
+	RegistryKey^ rk = Registry::LocalMachine->OpenSubKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon", false);
+	String^ shell = "Shell";
+	Object^ o = rk->GetValue(shell);
+	
+}
 
+void AtStart() {
+	GetInfo();
+	
 }
