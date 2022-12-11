b2fec6fc959aebbf1d7e4d235781cfbbdf8d0333
v38armageddon <69727502+v38armageddon@users.noreply.github.com>
2022-12-11 18:58:45 +0100

    Design du nouveau Bing Chrome

--- src/Bing Chrome/App.xaml.cs
+++ src/Bing Chrome/App.xaml.cs
@@ -8,10 +8,8 @@ using System.Windows;
 
 namespace Bing_Chrome
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        
     }
 }
