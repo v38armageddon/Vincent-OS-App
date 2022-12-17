45b0149327559cad9bcf511ffdad37e5abee2d63
v38armageddon <69727502+v38armageddon@users.noreply.github.com>
2022-12-17 13:58:06 +0100

    Design et création du nouveau Media Player

--- src/Vincent OS/WPF/Media.xaml.cs
+++ src/Vincent OS/WPF/Media.xaml.cs
@@ -11,17 +11,66 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using Windows.Media.Core;
+using Windows.Storage.Pickers;
+using Windows.Storage;
 
 namespace Vincent_OS.WPF
 {
-    /// <summary>
-    /// Logique d'interaction pour Media.xaml
-    /// </summary>
     public partial class Media : Window
     {
         public Media()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (My.MyProject.Forms.Bureau.Visible == true)
+            {
+                My.MyProject.Forms.Bureau.Button19.Visible = true;
+            }
+            else if (My.MyProject.Forms.Bureau2.Visible == true)
+            {
+                My.MyProject.Forms.Bureau2.Button19.Visible = true;
+            }
+        }
+
+        // Menu
+        private void ouvrirMusiqueMenu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async void ouvrirVideoMenu_Click(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker p = new FileOpenPicker();
+            p.FileTypeFilter.Add(".mp4");
+            p.FileTypeFilter.Add(".mkv");
+            p.FileTypeFilter.Add(".wmv");
+            p.FileTypeFilter.Add(".mov");
+            p.FileTypeFilter.Add(".avi");
+            StorageFile file = await p.PickSingleFileAsync();
+            if (file == null) return;
+            //var source = MediaSource.CreateFromStorageFile(file);
+            //mediaElement.Source = source;
+            //mediaElement.AutoPlay = true;
+        }
+
+        private void arreterMenu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void pleinEcranMenu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void quitterMenu_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
